221e08bc243ec32f4962c6867cf7d1c841804a84
Rishav Dhar <19497993+rdhar@users.noreply.github.com>
2023-02-07 01:08:11 +0000

    Add ec2 ubuntu instance

---
+++ main.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.53.0"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "learning-rdhar"
+  region  = "us-east-1"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"] # Canonical
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+
+  root_block_device {
+    volume_type = "gp2"
+    volume_size = 8 # GB
+  }
+}
