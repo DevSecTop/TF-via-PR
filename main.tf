7466af32f20c02c716ae88430c3088c6b365c49a
Rishav Dhar <19497993+rdhar@users.noreply.github.com>
2023-02-07 01:50:51 +0000

    Add eip to ec2 instance

--- main.tf
+++ main.tf
@@ -9,7 +9,24 @@ terraform {
 
 provider "aws" {
   profile = "learning-rdhar"
-  region  = "us-east-1"
+  region  = var.region
+}
+
+variable "region" {
+  type        = string
+  description = "AWS region where infrastructure will be based"
+  default     = "us-east-1"
+}
+
+variable "environment" {
+  type        = string
+  description = "Environment where infrastructure will be allocated"
+}
+
+variable "instance_type" {
+  type        = string
+  description = "AWS instance type to be used"
+  default     = "t2.micro"
 }
 
 data "aws_ami" "ubuntu" {
@@ -22,12 +39,33 @@ data "aws_ami" "ubuntu" {
   }
 }
 
-resource "aws_instance" "web" {
+resource "aws_instance" "app" {
   ami           = data.aws_ami.ubuntu.id
-  instance_type = "t2.micro"
+  instance_type = var.instance_type
 
   root_block_device {
-    volume_type = "gp2"
     volume_size = 8 # GB
+    volume_type = "gp2"
   }
+
+  tags = {
+    "Name"        = "${var.environment}-ubuntu-jammy"
+    "Project"     = "learning-rdhar"
+    "Environment" = var.environment
+    "Managed"     = "terraform"
+  }
+}
+
+resource "aws_eip" "app_eip" {
+  tags = {
+    "Name"        = "${var.environment}-ubuntu-jammy"
+    "Project"     = "learning-rdhar"
+    "Environment" = var.environment
+    "Managed"     = "terraform"
+  }
+}
+
+resource "aws_eip_association" "app_eip_assoc" {
+  instance_id   = aws_instance.app.id
+  allocation_id = aws_eip.app_eip.id
 }
