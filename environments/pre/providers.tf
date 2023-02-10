3d58a13d5f98dfad5595efb17e75fc73ab0c568c
Rishav Dhar <19497993+rdhar@users.noreply.github.com>
2023-02-10 19:31:54 +0000

    Add initial terraform workflow (#2)

---
+++ environments/pre/providers.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
+  }
+  backend "s3" {
+    bucket         = "terraform-aws-rdhar"
+    dynamodb_table = "terraform-aws-rdhar"
+  }
+}
+
+provider "aws" {}
