e6469dee78e26b7074717d1a09bfe6fcd92bc51f
Rishav Dhar <19497993+rdhar@users.noreply.github.com>
2023-02-11 04:02:07 +0000

    Fix deployment link

--- environments/pre/main.tf
+++ environments/pre/main.tf
@@ -1,31 +1,3 @@
-module "ec2_web" {
-  source          = "../../modules/compute"
-  environment     = var.environment
-  role            = "web"
-  instance_ami    = data.aws_ami.ubuntu.id
-  subnets         = module.vpc.vpc_public_subnets
-  security_groups = [module.vpc.security_group_public]
-  create_eip      = true
-
-  tags = {
-    Name = "${var.environment}-web"
-  }
-}
-
-module "ec2_worker" {
-  source          = "../../modules/compute"
-  environment     = var.environment
-  role            = "worker"
-  instance_ami    = data.aws_ami.ubuntu.id
-  subnets         = module.vpc.vpc_private_subnets
-  security_groups = [module.vpc.security_group_private]
-  create_eip      = false
-
-  tags = {
-    Name = "${var.environment}-worker"
-  }
-}
-
 module "vpc" {
   source          = "../../modules/network"
   environment     = var.environment
@@ -34,3 +6,31 @@ module "vpc" {
   public_subnets  = slice(cidrsubnets("10.0.0.0/17", 4, 4, 4, 4, 4, 4), 0, 3)
   private_subnets = slice(cidrsubnets("10.0.0.0/17", 4, 4, 4, 4, 4, 4), 3, 6)
 }
+
+# module "ec2_web" {
+#   source          = "../../modules/compute"
+#   environment     = var.environment
+#   role            = "web"
+#   instance_ami    = data.aws_ami.ubuntu.id
+#   subnets         = module.vpc.vpc_public_subnets
+#   security_groups = [module.vpc.security_group_public]
+#   create_eip      = true
+
+#   tags = {
+#     Name = "${var.environment}-web"
+#   }
+# }
+
+# module "ec2_worker" {
+#   source          = "../../modules/compute"
+#   environment     = var.environment
+#   role            = "worker"
+#   instance_ami    = data.aws_ami.ubuntu.id
+#   subnets         = module.vpc.vpc_private_subnets
+#   security_groups = [module.vpc.security_group_private]
+#   create_eip      = false
+
+#   tags = {
+#     Name = "${var.environment}-worker"
+#   }
+# }
