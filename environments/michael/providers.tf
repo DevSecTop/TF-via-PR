49c839a9c124cca2e0062825e65aaeaeb6e4dcb5
Rishav Dhar <19497993+rdhar@users.noreply.github.com>
2023-02-13 20:30:58 +0000

    Add demo commit (#20)

---
+++ environments/michael/providers.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
+  }
+  backend "s3" {}
+}
+
+provider "aws" {}
