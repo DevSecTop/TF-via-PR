e157b306a578bdc8928d7e84c4eea7d20c2cea7c
Rishav Dhar <19497993+rdhar@users.noreply.github.com>
2023-02-13 18:01:26 +0000

    Add demo environment

--- environments/pen/main.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_instance" "demo" {
-  ami           = "ami-0557a15b87f6559cf" # ubuntu-jammy-22.04-amd64
-  instance_type = "t2.micro"
-
-  tags = {
-    Terraform = terraform.workspace
-    Role      = "demo"
-  }
-}
