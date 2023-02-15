370388c2169b394c554c8236c373029821aaeaa3
Rishav Dhar <19497993+rdhar@users.noreply.github.com>
2023-02-15 19:36:43 +0000

    Demo prep

--- environments/bravo/providers.tf
+++ environments/bravo/providers.tf
@@ -1,15 +1,11 @@
 terraform {
+  backend "s3" {}
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 4.0"
     }
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.0"
-    }
   }
-  backend "s3" {}
 }
 
 provider "aws" {}
