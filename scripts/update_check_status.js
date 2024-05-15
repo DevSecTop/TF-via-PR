1d072a93b9865bcd5b7e88dfeacddbb97375ef44
Rishav Dhar <19497993+rdhar@users.noreply.github.com>
2024-05-15 10:30:01 +0100

    add initial draft with limited tests
    Signed-off-by: Rishav Dhar <19497993+rdhar@users.noreply.github.com>

---
+++ scripts/update_check_status.js
@@ -0,0 +1,13 @@
+module.exports = async ({ github, context }) => {
+  // Update the check status with the job summary before exiting.
+  const update_check_status = await github.rest.checks.update({
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    check_run_id: context.runId,
+    output: {
+      summary: "OUTPUT SUMMARY",
+      text: process.env.tf_summary,
+      title: "OUTPUT TITLE.",
+    },
+  });
+};
