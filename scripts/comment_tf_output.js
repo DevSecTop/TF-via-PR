3562a00350328cfc1a180af5490f047596b5e71c
Rishav Dhar <19497993+rdhar@users.noreply.github.com>
2024-05-19 18:24:34 +0100

    fix rogue `const`
    Signed-off-by: Rishav Dhar <19497993+rdhar@users.noreply.github.com>

--- scripts/comment_tf_output.js
+++ scripts/comment_tf_output.js
@@ -64,7 +64,7 @@ ${process.env.tf_fmt}
       .slice(0, 12000);
 
     // Create a collapsible summary of changes.
-    const comment_outline = process.env.changed_lines
+    comment_outline = process.env.changed_lines
       ? `<details><summary>Outline of changes.</summary>
 
 \`\`\`hcl
