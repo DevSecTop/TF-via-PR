53bf4e4f9d7611213af5f27b861169015b414b32
Rishav Dhar <19497993+rdhar@users.noreply.github.com>
2024-05-26 20:39:13 +0100

    debug print `process.env.tf_plan_outline`
    Signed-off-by: Rishav Dhar <19497993+rdhar@users.noreply.github.com>

--- scripts/comment_tf_output.js
+++ scripts/comment_tf_output.js
@@ -41,6 +41,7 @@ ${process.env.tf_fmt}
 
   // Parse the TFplan file to extract an outline of changes.
   let comment_outline = "";
+  console.log(process.env.tf_plan_outline);
   if (process.env.tf_plan_outline) {
     // Parse TFplan file.
     let tfplan = "";
