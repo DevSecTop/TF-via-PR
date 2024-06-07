a291fc81523de9254d5e1637a5ed8874417fdf4c
Rishav Dhar <19497993+rdhar@users.noreply.github.com>
2024-06-07 19:35:21 +0100

    revert ubuntu ami

--- stacks/sample_instance/main.tf
+++ stacks/sample_instance/main.tf
@@ -5,7 +5,7 @@ data "aws_ami" "ubuntu" {
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-noble-24.04-amd64-server-*"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
   }
 }
 
