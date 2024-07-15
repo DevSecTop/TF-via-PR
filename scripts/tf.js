cf960f16101c714e63db5cee3a5270b6d0bbece9
Rishav Dhar <19497993+RDhar@users.noreply.github.com>
2024-07-15 17:29:08 +0100

    fix fmt
    Signed-off-by: Rishav Dhar <19497993+RDhar@users.noreply.github.com>

--- scripts/tf.js
+++ scripts/tf.js
@@ -343,14 +343,17 @@ module.exports = async ({ context, core, exec, github }) => {
 ${tf_cli_input}
 \`\`\``;
 
-      const comment_fmt = tf_fmt_output.length
-        ? `<details><summary>Format diff check.</summary>
+      const comment_fmt =
+        process.env.arg_command === "plan" &&
+        /^true$/i.test(process.env.fmt_enable) &&
+        tf_fmt_output.length
+          ? `<details><summary>Format diff check.</summary>
 
 \`\`\`diff
 ${tf_fmt_output}
 \`\`\`
 </details>`
-        : "";
+          : "";
 
       const comment_outline = output_plan_outline.length
         ? `<details><summary>Outline of changes.</summary>
